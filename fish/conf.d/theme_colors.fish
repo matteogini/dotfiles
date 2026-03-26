3b94da73eb617d84636a38be4c67a4b4159d34ee
matteogini <matteogini@users.noreply.github.com>
2026-03-26 11:34:39 +0100

    temi

--- fish/conf.d/theme_colors.fish
+++ fish/conf.d/theme_colors.fish
@@ -1,10 +1,10 @@
 set -e fish_color_command
 set -e fish_color_param
-set -g fish_color_command #a8756c --bold
-set -g fish_color_param #8a706b
-set -g fish_color_quote #9e7770
-set -g fish_color_redirection #c65a47
-set -g fish_color_end #c65a47
+set -g fish_color_command #d88161 --bold
+set -g fish_color_param #b28474
+set -g fish_color_quote #cc8970
+set -g fish_color_redirection #ff4500
+set -g fish_color_end #ff4500
 set -g fish_color_error ff5555
-set -g fish_color_selection --background=#c65a47 --foreground=#1b2b34
+set -g fish_color_selection --background=#ff4500 --foreground=#1a1b26
 set -g fish_color_autosuggestion 555555
