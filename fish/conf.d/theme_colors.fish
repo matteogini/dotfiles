2193789dcb2fab84b70233f6e61b5dde55fc4afd
matteogini <matteogini@users.noreply.github.com>
2026-04-02 16:16:58 +0200

    waybar menu

--- fish/conf.d/theme_colors.fish
+++ fish/conf.d/theme_colors.fish
@@ -1,10 +1,10 @@
 set -e fish_color_command
 set -e fish_color_param
-set -g fish_color_command #d88161 --bold
-set -g fish_color_param #b28474
-set -g fish_color_quote #cc8970
-set -g fish_color_redirection #ff4500
-set -g fish_color_end #ff4500
+set -g fish_color_command ##ffffff --bold
+set -g fish_color_param #888888
+set -g fish_color_quote #aaaaaa
+set -g fish_color_redirection #ffffff
+set -g fish_color_end #ffffff
 set -g fish_color_error ff5555
-set -g fish_color_selection --background=#ff4500 --foreground=#1a1b26
+set -g fish_color_selection --background=#ffffff --foreground=#000000
 set -g fish_color_autosuggestion 555555
